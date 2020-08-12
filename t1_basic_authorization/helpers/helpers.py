37ac94d2f505bd22f6696bfb5537a7df53366908
MashaPast <mariepast13@gmail.com>
2020-08-12 19:06:50 +0300

    refactoring

--- t1_basic_authorization/helpers/helpers.py
+++ t1_basic_authorization/helpers/helpers.py
@@ -1,9 +1,9 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from t1_basic_authorization.config_loader import get_data
+from t1_basic_authorization.helpers.config_loader import get_data
 
-CONFIG_DATA = get_data("./t1_basic_authorization/config.json")
-TEST_DATA = get_data("./t1_basic_authorization/t_data.json")
+CONFIG_DATA = get_data("./t1_basic_authorization/configs/config.json")
+TEST_DATA = get_data("./t1_basic_authorization/test_data/test_data.json")
 
 
 def make_auth_get_request():
